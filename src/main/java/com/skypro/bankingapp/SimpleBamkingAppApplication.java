6c6fe76f1f98974fef4ab4335feec5ac1a6df236
Alexey Volkov <alexey.volkov@maxoptra.com>
2023-02-11 09:48:36 +0100

    Initial commit

---
+++ src/main/java/com/skypro/bankingapp/SimpleBamkingAppApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.bankingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBamkingAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SimpleBamkingAppApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/skypro/bankingapp/SimpleBamkingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.bankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBamkingAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
